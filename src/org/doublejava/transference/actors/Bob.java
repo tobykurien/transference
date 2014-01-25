7af81bd159c9207f6b180236384dc1b0f13c35a2
tobykurien <tobykurien@10.39.182.55>
2014-01-25 12:51:19 +0000

    walk/jump direction tweaks

--- src/org/doublejava/transference/actors/Bob.java
+++ src/org/doublejava/transference/actors/Bob.java
@@ -12,18 +12,26 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 public class Bob extends Actor {
    final int STATE_STOPPED = 0;
    final int STATE_WALKING = 1;
-   final int STATE_WALKING_BACK = 2;
    final int STATE_JUMP = 3;
 
    Texture texture;
    Animation walk;
    Animation walkBack;
    float stateTime = 0;
+   boolean forward = true;
    int state = STATE_STOPPED;
    int x = 0, y = 0;
    Rectangle bounds;
    private float velocity;
 
+   int jumpTime = 0;
+   float[] jumpX = new float[]{
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+   };
+   float[] jumpY = new float[]{
+      1, 1, 1, 1, 1, -1, -1, -1, -1, -1
+   };
+   
    public Bob(int initX, int initY) {
       texture = new Texture(Gdx.files.internal("data/bob_walk.png"));
       TextureRegion[][] regions = new TextureRegion(texture, texture.getWidth(), texture.getHeight()).split(16, 16);
@@ -38,41 +46,52 @@ public class Bob extends Actor {
 
    @Override
    public void draw(SpriteBatch batch, float parentAlpha) {
-      TextureRegion frame = (state==STATE_WALKING_BACK ? 
-               walkBack.getKeyFrame(stateTime, true) : 
-               walk.getKeyFrame(stateTime, true) );
+      TextureRegion frame = (forward ? 
+               walk.getKeyFrame(stateTime, true) : 
+               walkBack.getKeyFrame(stateTime, true) );
       batch.draw(frame, x, y);
    }
    
    @Override
    public Actor hit(float x, float y, boolean touchable) {
-      // TODO Auto-generated method stub
-      return super.hit(x, y, touchable);
+      if (bounds.contains(x, y)) {
+         return this;
+      } else {
+         return null;
+      }
    }
 
    @Override
    public void act(float deltaTime) {
       if (Gdx.input.isKeyPressed(Keys.D)) {
          state = STATE_WALKING;
+         forward = true;
          stateTime += deltaTime;
          x += 1;
       } else if (Gdx.input.isKeyPressed(Keys.A)) {
-         state = STATE_WALKING_BACK;
+         state = STATE_WALKING;
+         forward = false;
          stateTime += deltaTime;
          x -= 1;
-      } else if (Gdx.input.isKeyPressed(Keys.W)) {
-         state = STATE_JUMP;
-         stateTime = 0;
-         velocity = 10;
-      } else if (velocity == 0){
+      } else if (jumpTime == 0) {
          state = STATE_STOPPED;
          stateTime = 0;
+      } 
+
+      if (Gdx.input.isKeyPressed(Keys.W) && state != STATE_JUMP) {
+         state = STATE_JUMP;
+         stateTime = 0;
+         jumpTime = 0;
       }
       
       if (state == STATE_JUMP) {
-         velocity -= 0.1f;
-         x += velocity * 0.5f;
-         y += velocity * 0.5f;
+         int dir = (forward ? 1 : -1);
+         x += jumpX[jumpTime] * 5 * dir;
+         y += jumpY[jumpTime] * 15;
+         jumpTime += 1;
+         if (jumpTime >= jumpX.length) {
+            state = STATE_STOPPED;
+         }
       }
    }
 }
